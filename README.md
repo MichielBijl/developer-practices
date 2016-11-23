d5ca72d221c4194bf25bbeeb4dca8196fcdfe606
Michiel Bijl <github@michiel.moiety.me>
2016-11-23 00:53:03 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # Developer Practices
-
-This is the repository for Developer Practices. You're welcome to
-[[contribute]](CONTRIBUTING.md)!
---
+++ respec-config.js
@@ -0,0 +1,47 @@
+var respecConfig = {
+  // Embed RDFa data in the output.
+  doRDFa: '1.0',
+  includePermalinks: true,
+  permalinkEdge: true,
+  permalinkHide: false,
+
+  // Specification status
+  specStatus: 'unofficial',
+
+  noRecTrack: true,
+  diffTool: 'http://www.aptest.com/standards/htmldiff/htmldiff.pl',
+
+  // The specifications short name, as in http://www.w3.org/TR/short-name/
+  shortName: 'developer-practices-1.0',
+
+  // If you wish the publication date to be other than today,
+  // set publishDate.
+  // publishDate: "",
+  copyrightStart: '2016',
+
+  // If there is a previously published draft, uncomment this
+  // and set its YYYY-MM-DD date and its maturity status.
+  //
+  // previousPublishDate:  "",
+  // previousMaturity:  "",
+  // prevRecURI: "",
+  // previousDiffURI: "",
+
+  // If there a publicly available Editors Draft, this is the link
+  edDraftURI: 'https://MichielBijl.github.io/developer-practices/',
+
+  // If this is a LCWD, uncomment and set the end of its review period
+  // lcEnd: "2012-02-21",
+
+  // Editors, add as many as you like.
+  // “name” is the only required field.
+  editors: [
+    {
+      name: 'Michiel Bijl',
+      mailto: 'mbijl@paciellogroup.com',
+      company: 'The Paciello Group',
+      companyURI: 'https://www.paciellogroup.com/',
+      w3cid: 74040,
+    },
+  ],
+};
